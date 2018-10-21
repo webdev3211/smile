2e8ca876b41ff81df47cc441ee7376d2f447aa63
itsmrpk <pankaj031197@gmail.com>
2018-10-21 07:15:43 +0530

    changing email data

--- routes/volunteer.js
+++ routes/volunteer.js
@@ -1,5 +1,3 @@
-// ===
-// === === === === === === === === === === === === === === === ===
 var express = require('express');
 var router = express.Router();
 var User = require('../models/users');
@@ -15,29 +13,61 @@ var crypto = require("crypto");
 //=============================================
 //Post volunteering
 //=============================================
-router.post('/createVolunteer', (req, res, next) => {
+router.post('/:catId', (req, res, next) => {
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'webdev3211@gmail.com',
+            pass: 'simplepassword'
+        }
+    });
+
+
+
+
     var volunteer = new Volunteer({
-        category: req.body.category,
+        category: req.params.catId,
         name: req.body.name,
         address: req.body.address,
         contact: req.body.contact,
-        description: req.body.description,
+        description: req.body.description
     });
-
     volunteer.save().then(data => {
-            res.status(200).json({
-                success: true,
-                data: data,
-                message: 'Volunteerv form submitted successfully'
+
+            var mailOptions = {
+                from: 'webdev3211@gmail.com',
+                to: 'itsmrpk@gmail.com',
+                subject: 'You receive a request for social work',
+                text: `Hey someone has joined your hand
+                ${data}    
+            `
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    res.status(200).json({
+                        success: true,
+                        data: data,
+                        message: 'Volunteered form submitted succesfully'
+                    });
+                }
             });
+            //    res.status(200).json({
+            //        success:true,
+            //        data:data
+            //    });
         })
         .catch(err => {
             res.status(501).json({
                 success: false,
-                error: "Unable to Submit Volnteer form",
-                message: 'Volunteer form submitted could not be submitted'
+                error: "Unable to Submit",
+                message: 'Volunteered form could not get submitted try again later'
             });
-        });
+        })
 
 });
 
